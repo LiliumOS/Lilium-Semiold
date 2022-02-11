8557e81659715743f7314a1cd796c220b6330b9e
Connor Horman <chorman64@gmail.com>
2022-02-11 10:18:36 -0500

    Fix memcpy definition

--- include/memcpy.h
+++ include/memcpy.h
@@ -37,6 +37,6 @@ void* memmove(void* dst,const void* src,size_t sz);
 
 int memcmp(const void* a,const void* b,size_t sz);
 
-int memset(void* dst,int chr,size_t sz);
+void* memset(void* dst,int chr,size_t sz);
 
 #endif // defined(PHANTOMOS_MEMCPY_H_2020_06_28_14_53)
