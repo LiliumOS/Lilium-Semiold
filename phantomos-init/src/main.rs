d34cdc6fdccaf12cd80dcdf2c5eeff64dc8f40f6
Ray Redondo <rdrpenguin04@gmail.com>
2022-03-05 18:47:17 -0600

    [BROKEN] init: attempt to access terminal

--- phantomos-init/src/main.rs
+++ phantomos-init/src/main.rs
@@ -1,18 +1,31 @@
 #![no_std]
 #![no_main]
 
-use stivale_boot::v2::{StivaleHeader,StivaleStruct};
+use stivale_boot::v2::{StivaleHeader, StivaleStruct, StivaleTerminalHeaderTag};
 
 static STACK: [u8; 4096] = [0; 4096];
+static TAGS: &() = unsafe { &*make_tag_list() };
+
+const fn make_tag_list() -> *const () {
+    let mut next = core::ptr::null();
+    {
+        let next_temp = StivaleTerminalHeaderTag::new().next(next).flags(0);
+        next = (&next_temp as *const StivaleTerminalHeaderTag).cast();
+        core::mem::forget(next_temp);
+    }
+    next
+}
 
 #[link_section = ".stivale2hdr"]
 #[no_mangle]
 #[used]
 static STIVALE_HDR: StivaleHeader = StivaleHeader::new()
-    .stack(&STACK[4095] as *const u8);
+    .stack(&STACK[4095] as *const u8)
+    .tags(TAGS as *const ());
 
 #[no_mangle]
-extern "C" fn _start(_header_addr: *const StivaleStruct) -> ! {
+extern "C" fn _start(stivale_data: *const StivaleStruct) -> ! {
+    
     loop {}
 }
 
