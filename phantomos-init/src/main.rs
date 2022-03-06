8216df308e7c1dfb5fceda078fe83cb9d27cde85
Ray Redondo <rdrpenguin04@gmail.com>
2022-03-05 19:31:13 -0600

    [BROKEN]

--- phantomos-init/src/main.rs
+++ phantomos-init/src/main.rs
@@ -4,28 +4,22 @@
 use stivale_boot::v2::{StivaleHeader, StivaleStruct, StivaleTerminalHeaderTag};
 
 static STACK: [u8; 4096] = [0; 4096];
-static TAGS: &() = unsafe { &*make_tag_list() };
-
-const fn make_tag_list() -> *const () {
-    let mut next = core::ptr::null();
-    {
-        let next_temp = StivaleTerminalHeaderTag::new().next(next).flags(0);
-        next = (&next_temp as *const StivaleTerminalHeaderTag).cast();
-        core::mem::forget(next_temp);
-    }
-    next
-}
+static TERMINAL_HEADER_TAG: StivaleTerminalHeaderTag = StivaleTerminalHeaderTag::new().flags(0);
 
 #[link_section = ".stivale2hdr"]
 #[no_mangle]
 #[used]
 static STIVALE_HDR: StivaleHeader = StivaleHeader::new()
     .stack(&STACK[4095] as *const u8)
-    .tags(TAGS as *const ());
+    .tags(&TERMINAL_HEADER_TAG as *const StivaleTerminalHeaderTag as *const ());
 
+#[allow(clippy::empty_loop)]
 #[no_mangle]
 extern "C" fn _start(stivale_data: *const StivaleStruct) -> ! {
-    
+    let stivale_data = unsafe { stivale_data.as_ref().unwrap_unchecked() };
+    let terminal = stivale_data.terminal().unwrap();
+    let term_write = terminal.term_write();
+    term_write("Initializing PhantomOS...");
     loop {}
 }
 
