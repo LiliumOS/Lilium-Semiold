be1d2520085e77cae3af1f5778f4997cd6151821
Connor Horman <chorman64@gmail.com>
2022-03-24 23:39:24 -0400

    Start defining PhantomFS data structures

--- phantom-filesystem-drivers/src/phantomfs.rs
+++ phantom-filesystem-drivers/src/phantomfs.rs
@@ -1,1 +1,61 @@
+use core::num::{NonZeroU32, NonZeroU64};
 
+use bytemuck::{Pod, Zeroable};
+
+bitflags::bitflags! {
+    #[derive(Default,Zeroable,Pod)]
+    #[repr(transparent)]
+    pub struct PhantomFSObjectFlags : u32{
+
+    }
+}
+
+bitflags::bitflags! {
+    #[derive(Default,Zeroable,Pod)]
+    #[repr(transparent)]
+    pub struct PhantomFSStreamFlags : u64 {
+        const REQUIRED       = 0x0000000000000001;
+        const WRITE_REQUIRED = 0x0000000000000002;
+        const ENUM_REQUIRED  = 0x0000000000000004;
+    }
+}
+
+fake_enum::fake_enum! {
+    #[repr(u16)]
+    #[derive(Hash,Zeroable,Pod)]
+    pub enum struct PhantomFSObjectType{
+        Regular = 0,
+        Directory = 1,
+        Symlink = 2,
+        Fifo = 3,
+        Socket = 4,
+        BlockDeivce = 5,
+        CharDevice = 6,
+        CustomType = 65535
+    }
+}
+
+#[repr(C, align(64))]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, Zeroable, Pod)]
+pub struct PhantomFSObject {
+    strong_ref: u32,
+    weak_ref: Option<NonZeroU32>,
+    streams_size: u64,
+    streams_ref: u128,
+    streams_indirection: u8,
+    reserved33: [u8; 5],
+    ty: PhantomFSObjectType,
+    flags: PhantomFSObjectFlags,
+    reserved44: [u8; 20],
+}
+
+#[repr(C, align(128))]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, Zeroable, Pod)]
+pub struct StreamListing {
+    name: [u8; 32],
+    name_ref: Option<NonZeroU64>,
+    flags: PhantomFSStreamFlags,
+    size: u64,
+    reserved: [u64; 3],
+    inline_data: [u8; 48],
+}
--- phantomos-kernel-std/src/io.rs
+++ phantomos-kernel-std/src/io.rs
@@ -1,34 +1,93 @@
-pub enum SeekFarFrom {
-    Start(u128),
-    End(i128),
-    Current(i128),
-    SectorStart(u128),
-}
-
 pub enum SeekFrom {
     Start(u64),
     End(i64),
     Current(i64),
+    StartFar(u128),
+    EndFar(i128),
+    CurrentFar(i128),
 }
 
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Error {
     // Every io operation is currently infallible, yay!
+    UnexpectedEof,
+    Interrupted,
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        match self {
+            Self::UnexpectedEof => f.write_str("Unexpected End of File"),
+            Self::Interrupted => f.write_str("Operation Interrupted"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<rust_std::io::Error> for Error {
+    fn from(err: rust_std::io::Error) -> Error {
+        match err.kind() {
+            _ => todo!(),
+        }
+    }
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
 
 pub trait Read {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize>;
+
+    fn read_exact(&mut self, mut buf: &mut [u8]) -> Result<()> {
+        loop {
+            match self.read(buf) {
+                Ok(0) => break Err(Error::UnexpectedEof),
+                Ok(n) if n == buf.len() => {
+                    break Ok(());
+                }
+                Ok(n) => {
+                    buf = &mut buf[n..];
+                }
+                Err(Error::Interrupted) => continue,
+                Err(e) => break Err(e),
+            }
+        }
+    }
+
+    fn by_ref(&mut self) -> &mut Self
+    where
+        Self: Sized,
+    {
+        self
+    }
+}
+
+impl<R: Read> Read for &mut R {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        <R as Read>::read(self, buf)
+    }
 }
 
 pub trait Seek {
     fn seek(&mut self, pos: SeekFrom) -> Result<usize>;
 }
 
-pub trait SeekFar {
-    fn seek_far(&mut self, pos: SeekFarFrom) -> Result<u128>;
+impl<S: Seek> Seek for &mut S {
+    fn seek(&mut self, pos: SeekFrom) -> Result<usize> {
+        <S as Seek>::seek(self, pos)
+    }
 }
 
 pub trait Write {
     fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+}
+
+impl<W: Write> Write for &mut W {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        <W as Write>::write(self, buf)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        <W as Write>::flush(self)
+    }
 }
